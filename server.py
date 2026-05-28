46f6e9a16339952027d672a5ebb395ab1f07ca20
Kowshal <hm.kowshal@gmail.com>
2026-05-28 17:58:04 +0530

    updated mcp server code file

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from fastmcp import FastMCP
 import random
+import json
 
 
 mcp = FastMCP(name="Simple Remote Server.")
@@ -15,6 +16,18 @@ def generate_random_number(start_range: int,stop_range: int)->int:
     """Generate a random number between the given start_range and stop_range."""
     return random.randint(start_range,stop_range)
 
+@mcp.resource("info://server")
+def server_info()->str:
+    """Get information about this server"""
+    info={
+        "name":"Simple Calculator Server",
+        "version":"1.0.0",
+        "description":"A basic MCP server with math tools.",
+        "tools":["add","random_number"],
+        "author":"Veeru"
+    }
+    return json.dumps(info,indent=2)
+
 
 if __name__=="__main__":
-    mcp.run()
+    mcp.run(transport="http",host="0.0.0.0",port=8000)
