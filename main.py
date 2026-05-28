5c680299d6131177e45589b683294c5f452d2416
Kowshal <hm.kowshal@gmail.com>
2026-05-28 17:35:44 +0530

    mcp server code file

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from simple-mcp-server!")
+from fastmcp import FastMCP
+import random
 
 
-if __name__ == "__main__":
-    main()
+mcp = FastMCP(name="Simple Remote Server.")
+
+@mcp.tool
+def add(a: int,b: int)->int:
+    """Adds two given numbers a and b and returns its sum."""
+    return a+b
+
+
+@mcp.tool
+def generate_random_number(start_range: int,stop_range: int)->int:
+    """Generate a random number between the given start_range and stop_range."""
+    return random.randint(start_range,stop_range)
+
+
+if __name__=="__main__":
+    mcp.run()
